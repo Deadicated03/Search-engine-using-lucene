0278f9c29f94c33607cdd9fa6bf00f5eabd150cc
Konstantinos Chatziioannou <146972055+Deadicated03@users.noreply.github.com>
2024-05-21 19:45:06 +0300

    Update Parser.java

--- src/main/java/lucPackage/Parser.java
+++ src/main/java/lucPackage/Parser.java
@@ -6,15 +6,12 @@ import java.io.IOException;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 
-import java.io.Reader;
-
-
 public class Parser {
    
 	  public CSVParser parser(String csvFilePath) throws IOException {  
-	        
+	        // Directly parse the file using CSVParser
 	        FileReader reader = new FileReader(csvFilePath);
-	        CSVFormat csvFormat = CSVFormat.DEFAULT;  
+	        CSVFormat csvFormat = CSVFormat.DEFAULT;  // Assuming the first record is a header
 	        CSVParser csvParser = new CSVParser(reader, csvFormat);
 	        return csvParser; 
 	    }
